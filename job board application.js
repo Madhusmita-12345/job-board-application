75f50ece7e4f796ac9a97f3511562d5409632203
Madhusmita-12345 <madhusmitasahoopiji01@gmail.com>
2025-02-17 16:09:54 +0530

    Add files via upload

---
+++ job board application.js
@@ -0,0 +1,25 @@
+// server/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+    
+});
